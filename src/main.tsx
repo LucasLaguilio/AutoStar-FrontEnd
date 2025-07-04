9da904b4e61f839e0f4b414d9554458e7d34dbaa
ElifasPF <elifas.faustino@estudante.ifms.edu.br>
2025-07-04 17:48:34 -0400

    implementação do cadastro de clientes

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './main.css'
 import Header from './Header.tsx'
 import Cadastro from './Cadastro.tsx'
+import CadastroClientes from './Clientes.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Header/>
     <Cadastro/>
+    <CadastroClientes/>
   </StrictMode>,
 )
